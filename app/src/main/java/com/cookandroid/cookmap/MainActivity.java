1d85d156b637e07e30d4cd70f9391886388e3872
zeenee <jinnnih@gmail.com>
2026-05-14 15:48:31 +0900

    feat: 실습 과제 구글지도 경기도 맛집 리스트

--- app/src/main/java/com/cookandroid/cookmap/MainActivity.java
+++ app/src/main/java/com/cookandroid/cookmap/MainActivity.java
@@ -1,90 +1,108 @@
 package com.cookandroid.cookmap;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.SubMenu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap gMap;
     SupportMapFragment mapFrag;
+    GroundOverlayOptions placeMark;
+    Button btnPrev, btnNext;
+    List<String> lines = new ArrayList<>();
+    int placeCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Google 지도");
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.ic_pin_blue);
-
+        setTitle("경기도 으뜸 맛집");
         mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
-    }
 
-    @Override
-    public void onMapReady(GoogleMap map) {
-        gMap = map;
-        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 13));
-        gMap.getUiSettings().setZoomControlsEnabled(true);
-        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+        readCSV();
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapClick(@NonNull LatLng point) {
-                gMap.addMarker(new MarkerOptions()
-                        .position(point)
-                        .title("클릭 위치")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+            public void onClick(View view) {
+                String line = lines.get(placeCount++);
+                if (placeCount > lines.size() - 1)
+                    placeCount = 0;
+                String[] tokens = line.split(",");
+                double lat = Double.parseDouble(tokens[0]);
+                double lon = Double.parseDouble(tokens[1]);
+                String restName = tokens[2];
+
+                LatLng point = new LatLng(lat, lon);
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 13));
+                placeMark = new GroundOverlayOptions()
+                        .image(BitmapDescriptorFactory.fromResource(R.drawable.food))
+                        .position(point, 500f, 500f);
+                gMap.addGroundOverlay(placeMark);
+                Toast.makeText(getApplicationContext(), restName, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String line = lines.get(placeCount--);
+                if (placeCount < 0)
+                    placeCount = lines.size() - 1;
+                String[] tokens = line.split(",");
+                double lat = Double.parseDouble(tokens[0]);
+                double lon = Double.parseDouble(tokens[1]);
+                String restName = tokens[2];
+
+                LatLng point = new LatLng(lat, lon);
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 13));
+                placeMark = new GroundOverlayOptions()
+                        .image(BitmapDescriptorFactory.fromResource(R.drawable.food))
+                        .position(point, 500f, 500f);
+                gMap.addGroundOverlay(placeMark);
+                Toast.makeText(getApplicationContext(), restName, Toast.LENGTH_LONG).show();
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        menu.add(0, 1, 0, "위성지도");
-        menu.add(0, 2, 0, "일반지도");
-        menu.add(0, 3, 0, "월드컵경기장 바로가기");
-
-        SubMenu sMenu = menu.addSubMenu("유명한 장소 바로가기 >>");
-        sMenu.add(0, 4, 0, "제주도 성산일출봉");
-        sMenu.add(0, 5, 0, "광주 무등산");
-        sMenu.add(0, 6, 0, "서울 경복궁");
-        return true;
+    public void onMapReady(GoogleMap map) {
+        gMap = map;
+        gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 13));
+        gMap.getUiSettings().setZoomControlsEnabled(true);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case 1:
-                gMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                return true;
-            case 2:
-                gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                return true;
-            case 3:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.658256, 126.897240), 13));
-                return true;
-            case 4:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(33.458333, 126.941944), 13));
-                return true;
-            case 5:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.132222, 126.988333), 13));
-                return true;
-            case 6:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.579617, 126.977041), 13));
-                return true;
-        }
-        return false;
+    public void readCSV() {
+        InputStream inputStream = getResources().openRawResource(R.raw.good_place);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String line;
+            line = reader.readLine(); // 첫 제목행을 읽어서 버림
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            reader.close();
+        } catch (Exception e) {}
     }
 }
