d07f298fb7b47bcf83d2a742a26b0d9405d4798e
zeenee <jinnnih@gmail.com>
2026-05-14 13:00:25 +0900

    feat: 실습 과제 구글 위성지도 추가

--- app/src/main/java/com/cookandroid/cookmap/MainActivity.java
+++ app/src/main/java/com/cookandroid/cookmap/MainActivity.java
@@ -1,20 +1,22 @@
 package com.cookandroid.cookmap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap gMap;
-    MapFragment mapFrag;
-    GroundOverlayOptions videoMark;
+    SupportMapFragment mapFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +24,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         setTitle("구글지도 활용");
 
-        mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -37,5 +33,37 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         gMap = map;
         gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 13));
+        gMap.getUiSettings().setZoomControlsEnabled(true);
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng point) {
+                gMap.addMarker(new MarkerOptions().position(point).title("클릭 위치"));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, 1, 0, "위성지도");
+        menu.add(0, 2, 0, "일반지도");
+        menu.add(0, 3, 0, "월드컵경기장 바로가기");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                gMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                return true;
+            case 2:
+                gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case 3:
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.658256, 126.897240), 13));
+                return true;
+        }
+        return false;
     }
 }
